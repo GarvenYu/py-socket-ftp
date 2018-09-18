c00f50885f61302dc22edf63231e21cd2a51d14b
GarvenYu <1747603726@qq.com>
2018-09-18 23:22:32 +0800

    code tcp-server

--- env/socket-server/tcpserver.py
+++ env/socket-server/tcpserver.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import socket
-
+import select
 
 class TcpServer(object):
 
@@ -24,7 +24,22 @@ class TcpServer(object):
         server_socket.bind((self.host, self.port))
         server_socket.setblocking(False)
         server_socket.listen(5)
+        epoll = select.epoll()
+        epoll.register(server_socket.fileno(), select.EPOLLIN)
+        fd_to_socket = {server_socket.fileno() : server_socket}
         # 打印日志
+        while True:
+            # 打印日志
+            # 轮询注册的事件集合，返回值为[(文件句柄，对应的事件)，(...),....]
+            events = epoll.poll(self.time_out)
+            if not events:
+                # 打印日志
+                continue
+            # 打印日志
+            # 进行处理
+            for fd, event in events:
+                pass
+
 
 tcpServer = TcpServer('', 8001, 10, 10)
 tcpServer.start_server()
